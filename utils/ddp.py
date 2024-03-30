420aeb735877eb7347d01c74efa88fb535899a9b
Facebear-ljx <1076231926@qq.com>
2024-03-30 11:38:48 +0800

    ddp setup port

--- utils/ddp.py
+++ utils/ddp.py
@@ -5,14 +5,14 @@ from torch.distributed import init_process_group, destroy_process_group
 
 
 
-def ddp_setup(rank: int, world_size: int, verbose: bool=False):
+def ddp_setup(rank: int, world_size: int, verbose: bool=False, port: str="12355"):
        """
        Args:
        rank: Unique identifier of each process
        world_size: Total number of processes
        """
        os.environ["MASTER_ADDR"] = "localhost"
-       os.environ["MASTER_PORT"] = "12355" 
+       os.environ["MASTER_PORT"] = port
        torch.cuda.set_device(rank)
        init_process_group(backend="nccl", rank=rank, world_size=world_size)
        torch.distributed.barrier()
