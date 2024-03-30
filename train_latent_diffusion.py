eb1cd78f9e8860dfb3c2f8a7e3d290cdbf7a167b
Facebear-ljx <1076231926@qq.com>
2024-03-30 17:50:31 +0800

    main.py for latent diffusion

--- train_latent_diffusion.py
+++ train_latent_diffusion.py
@@ -30,11 +30,18 @@ def get_args():
        parser.add_argument('--visual_pretrain', default=True, type=boolean, help='whether use visual pretrain')
        parser.add_argument('--steps', default=int(1e+6), type=float, help='train steps')
        parser.add_argument("--seed", default=42, type=int)  # Sets PyTorch and Numpy seeds
-       parser.add_argument('--batch_size', default=128, type=int)
+       parser.add_argument('--batch_size', default=512, type=int)
        parser.add_argument('--lr', default=1e-4, type=float, help='learning rate')
-       parser.add_argument('--save', default=False, type=boolean, help='save ckpt or not')
+       parser.add_argument('--save', default=True, type=boolean, help='save ckpt or not')
        parser.add_argument('--save_freq', default=int(1e+4), type=int, help='save ckpt frequency')
        parser.add_argument('--resume', default="None", type=str, help='resume path')
+
+       # diffusion model
+       parser.add_argument('--num_blocks', default=6, type=int, help='image size')
+       parser.add_argument('--hidden_dim', default=1024, type=int, help='image size')
+       parser.add_argument('--T', default=100, type=int, help='maximize diffusion time steps')
+       parser.add_argument('--time_embed', default='learned', type=str, help='learned or fixed type time embedding')
+       parser.add_argument('--beta', default='vp', type=str, help='noise schedule')
 
        # encoder type
        parser.add_argument("--encoder", default="DecisionNCE-T", type=str, help="choose your encoder")
@@ -46,7 +53,7 @@ def get_args():
        parser.set_defaults(pin_mem=True)
        
        # distributed training parameters
-       parser.add_argument('--world_size', default=1, type=int, help='number of distributed processes')
+       parser.add_argument('--world_size', default=4, type=int, help='number of distributed processes')
        parser.add_argument('--port', default="11111", type=str, help='port for ddp')
        args = parser.parse_args()    
        return args   
@@ -69,12 +76,19 @@ def main(rank: int, world_size: int, save_path: str, args):
        )
        
        # agent and the model for agent
-       encoder = DecisionNCE_encoder(args.encoder)
+       encoder = DecisionNCE_encoder(args.encoder, device=rank)
        cond_dim = encoder.v_dim * args.frames + encoder.l_dim
        input_dim = encoder.v_dim
        output_dim = encoder.v_dim
-       vpmodel = IDQLDiffusion(input_dim, output_dim, cond_dim, device=rank).to(rank)
-       vpagent = DDPM_BC_latent(vpmodel, schedule='vp', num_timesteps=50, mmencoder=encoder)      
+       vpmodel = IDQLDiffusion(input_dim, 
+                               output_dim, 
+                               cond_dim, 
+                               time_dim=args.hidden_dim,
+                               time_embeding=args.time_embed, 
+                               num_blocks=args.num_blocks,
+                               hidden_dim=args.hidden_dim,
+                               device=rank).to(rank)
+       vpagent = DDPM_BC_latent(vpmodel, schedule=args.beta, num_timesteps=args.T, mmencoder=encoder)      
 
        # # trainer
        test_trainer = BCTrainer(vpagent, 
@@ -103,22 +117,20 @@ if __name__ == '__main__':
        import os
        from datetime import datetime
 
-       # previous_dir = os.getcwd()
-       # base_dir = "experiments"
-       # project_name = args.project_name
-       # data_name = args.dataset_name
-       # time = datetime.now()
-       # time = time.strftime("%Y-%m-%d %H:%M:%S")
-       # save_path = f"{previous_dir}/{base_dir}/{project_name}/{data_name}/{time}"
-       save_path = None
-       
-       # if not os.path.exists(save_path):
-              # os.makedirs(save_path)
+       previous_dir = os.getcwd()
+       base_dir = "experiments"
+       project_name = args.project_name
+       data_name = args.dataset_name
+       time = datetime.now()
+       time = time.strftime("%Y-%m-%d %H:%M:%S")
+       save_path = f"{previous_dir}/{base_dir}/{project_name}/{data_name}/{time}"
+       if not os.path.exists(save_path):
+              os.makedirs(save_path)
        
        # seed
        seed = args.seed + ddp.get_rank()
        np.random.seed(seed)
        torch.manual_seed(seed)
        random.seed(seed)
-       main(0, 1, None, args)
-       # mp.spawn(main, args=(args.world_size, save_path, args), nprocs=args.world_size)
+
+       mp.spawn(main, args=(args.world_size, save_path, args), nprocs=args.world_size)
