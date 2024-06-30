74ce1252042a05bd1636366cec652e0fc91476cb
Facebear-ljx <1076231926@qq.com>
2024-06-30 17:41:29 +0800

    merge

--- train_example/run_diffusion_libero.sh
+++ train_example/run_diffusion_libero.sh
@@ -1,5 +1,7 @@
 bs=64
 ws=2
+text_encoder=DecisionNCE-T
+for seed in 42; do
 export CUDA_VISIBLE_DEVICES=1
 export WANDB_API_KEY=cfbf81ce9bd7daca9d32f4bd1dbf26e8c93310c3
 torchrun --standalone --nnodes=1 --nproc-per-node=1 train_diffusion_policy_example_libero.py \
@@ -7,9 +9,10 @@ torchrun --standalone --nnodes=1 --nproc-per-node=1 train_diffusion_policy_examp
     --algo_name 'diffusion visual motor' \
     --ddp False \
     --img_size 128 \
-    --visual_encoder resnet50 \
+    --seed $seed \
+    --visual_encoder resnet34 \
     --visual_pretrain True \
-    --text_encoder DecisionNCE-T \
+    --text_encoder $text_encoder \
     --ft_vision True \
     --film_fusion False \
     --ac_num 6 \
@@ -27,10 +30,11 @@ torchrun --standalone --nnodes=1 --nproc-per-node=1 train_diffusion_policy_examp
     --eval_freq 25000 \
     --resume None \
     --wandb True \
-    --steps 2000000 \
+    --steps 200000 \
     --save True \
-    --save_freq 25000 \
+    --save_freq 50000 \
     --T 25 \
-    --save_path ../experiments/libero/libero_goal/diffusion/resnet34_wstate_0613 \
-    --log_path ../experiments/libero/libero_goal/diffusion/resnet34_wstate_0613 \
-    --port 2050 \
+    --save_path ../experiments/libero/libero_goal/diffusion/resnet34_wstate_0613_$text_encoder \
+    --log_path ../experiments/libero/libero_goal/diffusion/resnet34_wstate_0613_$text_encoder \
+    --port 2050
+done
